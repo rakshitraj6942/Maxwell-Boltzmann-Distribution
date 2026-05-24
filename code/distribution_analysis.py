32cb04d992eefb8d7a766e38b67c4d5b6ad35554
Rakshit Raj <rajrakshit212@gmail.com>
2026-05-24 20:12:31 +0530

    Implemented T and m as arguments of plot function

--- code/distribution_analysis.py
+++ code/distribution_analysis.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 # Constants (K and kg)
-T = 300 ; m = 4.65e-26
+T_gl = 300 ; m_gl = 4.65e-26
 k_B = 1.380649e-23  # Boltzmann Constant (J/K)
 
 # ---------- Velocities and Speed -------------
@@ -11,7 +11,7 @@ velocity_comp = generate_velocities(N=100000, T=T, m=m)
 speeds = compute_speeds(velocities=velocity_comp)
 
 
-def plots(speeds: np.ndarray):
+def plots(speeds: np.ndarray, T, m):
     '''
     Plots normalized histogram for speeds such that it approximates a probability density function for v (f(v)).
     '''
@@ -29,4 +29,4 @@ def plots(speeds: np.ndarray):
     plt.title(f'Comparison of Experimental and Theoretical Plots for N={len(speeds)}')
     plt.show()
 
-plots(speeds)
+plots(speeds, T=T_gl, m=m_gl)
